cedd4d0b0bff3319b592e539a2e8c5c8e4a36d8b
sumit <sumit2408@gmail.com>
2018-07-19 23:12:46 +0530

    changed port number to 4446

--- src/test/java/gui/facebook/stepfiles/Hooks.java
+++ src/test/java/gui/facebook/stepfiles/Hooks.java
@@ -30,13 +30,13 @@ public class Hooks {
 		if(browser.equalsIgnoreCase("chrome")){
 			//driver = new ChromeDriver();
 			DesiredCapabilities cap = DesiredCapabilities.chrome();
-			driver = new RemoteWebDriver(new URL("http://54.146.174.3:32768/wd/hub"), cap);
+			driver = new RemoteWebDriver(new URL("http://54.146.174.3:4446/wd/hub"), cap);
 		}
 		
 		else if(browser.equalsIgnoreCase("ff")){
 			//driver = new FirefoxDriver();
 			DesiredCapabilities cap = DesiredCapabilities.firefox();
-			driver = new RemoteWebDriver(new URL("http://127.0.0.1:32769/wd/hub"), cap);
+			driver = new RemoteWebDriver(new URL("http://54.146.174.3:4446/wd/hub"), cap);
 		}
 		
 		else{
